08270f428e81ab206c46d33c74a0812c045f1b0d
Atsushi-Hasegawa <pianoidea@yahoo.co.jp>
2015-08-25 23:13:26 +0900

    エラーページを一定時間で画像が変化するように修正

--- 404.php
+++ 404.php
@@ -1,22 +1,35 @@
 <!DOCTYPE HTML>
 <html>
 <head>
- <meta http-equiv="Content-Type" content="text/html; charset=iso-2022-jp">
-<H1>404 Not Found</H1></head>
+ <meta charset='utf-8'>
+ <link rel="stylesheet" href="http://localhost:8081/BBS/css/bootstrap.min.css">
+ <link rel="stylesheet" href="http://localhost:8081/BBS/css/bootstrap-theme.min.css">
+ <link rel="stylesheet" href="http://localhost:8081/BBS/template/css/stylesheet.css">
+<div class="page-header center"><H1>404 Not Found</H1></div>
+</head>
 <body>
-<?php 
-  $image_list = array(
-                     "http://pds.exblog.jp/pds/1/200809/14/22/c0137122_63091.jpg",
-                     "http://blog-imgs-31.fc2.com/z/o/o/zooon/sawako2.jpg",
-                     "http://dic.nicovideo.jp/oekaki/728705.png",
-                     "http://blog-imgs-11.fc2.com/k/u/r/kuroshimatubaki/miochan01.gif",
-                     "http://blog-imgs-37.fc2.com/a/n/i/animezyouhou662/khgk.jpg"
-                    );
-  $index = rand(0,4);
-  $image = $image_list[$index];
-?>
-  <img src=<?php echo $image;?> width="128" height="128"><br>
-ファイルが削除されたか、サイトが閉鎖されたため指定されたページが見つかりません。
+<div class="center">
+<img src="http://pds.exblog.jp/pds/1/200809/14/22/c0137122_63091.jpg" name="error_img" width="256" height="256">
+<script type="text/javascript">
+  img = new Array(
+    "http://pds.exblog.jp/pds/1/200809/14/22/c0137122_63091.jpg",
+    "http://blog-imgs-31.fc2.com/z/o/o/zooon/sawako2.jpg",
+    "http://dic.nicovideo.jp/oekaki/728705.png",
+    "http://blog-imgs-11.fc2.com/k/u/r/kuroshimatubaki/miochan01.gif",
+    "http://blog-imgs-37.fc2.com/a/n/i/animezyouhou662/khgk.jpg"
+  );
+  var img_num = -1;
+  display_img();
+  function display_img()
+  {
+    img_num++;
+    if(img_num == img.length) img_num = 0;
+    document.error_img.src = img[img_num];
+    setTimeout("display_img()", 1000);
+  }
+</script><br><br>
+ファイルが削除されたか、サイトが閉鎖されたため指定されたページが見つかりません。<br><br>
 <a href="index.php?url=login">戻る</a>
+</div>
 </body>
 </html>
