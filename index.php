64a7d38132105dbf934cfc39cae8edb93ab17746
Atsushi-Hasegawa <pianoidea@yahoo.co.jp>
2015-09-03 08:50:28 +0900

    不要な変数を削除

--- index.php
+++ index.php
@@ -10,17 +10,16 @@ session_start();
 session_regenerate_id(true);
 $url = $_GET['url'];
 $page = isset($_GET['page']) ? $_GET['page'] : 1;
-$params = @explode("/" , $_GET["url"]);
-$test  = ucwords(array_shift($params));
-$thread_id = null;
 $path = __DIR__ . "/controllers/${url}.php";
 if(!is_file($path))
 {
   header("HTTP/1.1 404 Not Found");
   header("Location: 404.php");
+  exit;
 }
 
 $format_url = ucwords($url);
 require_once($path);
 $controller = new $format_url($page);
 $controller->execute();
+exit
